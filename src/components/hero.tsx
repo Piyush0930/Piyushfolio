ca24e4f213225cfe329445eb95aee40329ba6c5a
Piyush Patil <piyushpatil922004@gmail.com>
2025-08-06 14:58:57 +0000

    on home page keep the detail slightly down

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -17,7 +17,7 @@ export function Hero() {
   return (
     <section id="home" className="py-8">
        <BackgroundBeamsWithCollision className="absolute top-0 left-0 w-full h-full z-0" />
-      <div className="relative z-10 text-center mt-0 mb-16">
+      <div className="relative z-10 text-center mt-20 mb-16">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center mx-auto">
           <TextGenerateEffect
             words={userProfile.name}
