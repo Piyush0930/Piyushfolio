be7b0381ffb28641fd8aef7549a78cfb696eed9b
Piyush Patil <piyushpatil922004@gmail.com>
2025-08-06 14:36:57 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <TextGenerateEffec

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -19,11 +19,11 @@ export function Hero() {
        <BackgroundBeamsWithCollision className="absolute top-0 left-0 w-full h-full z-0" />
       <div className="relative z-10 text-center my-20">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center mx-auto">
+          <TextGenerateEffect words={userProfile.name} className="text-center text-[40px] md:text-5xl lg:text-6xl" />
+          
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic Web Magic with Next.js
           </h2>
-
-          <TextGenerateEffect words={userProfile.name} className="text-center text-[40px] md:text-5xl lg:text-6xl" />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             {userProfile.title}
