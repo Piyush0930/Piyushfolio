3136e201a075cfa0e9b9e7dc36593c9936d4b388
Piyush Patil <piyushpatil922004@gmail.com>
2025-08-06 14:36:09 +0000

    You are given a task to integrate a React component into your codebase.

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,8 +5,8 @@ import React from "react";
 import { Button } from "./ui/button";
 import { Download, Linkedin, Github } from "lucide-react";
 import Link from "next/link";
-import { Spotlight } from "./ui/spotlight";
 import { TextGenerateEffect } from "./ui/text-generate-effect";
+import { BackgroundBeamsWithCollision } from "./ui/background-beams-with-collision";
 
 const iconMap: { [key: string]: React.ElementType } = {
   Linkedin,
@@ -15,23 +15,8 @@ const iconMap: { [key: string]: React.ElementType } = {
 
 export function Hero() {
   return (
-    <section id="home" className="pb-20 pt-36">
-      <div>
-        <Spotlight
-          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-          fill="white"
-        />
-        <Spotlight
-          className="top-10 left-full h-[80vh] w-[50vw]"
-          fill="purple"
-        />
-        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
-      </div>
-
-      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black/[0.2] absolute top-0 left-0 flex items-center justify-center">
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
-      </div>
-      
+    <section id="home" className="py-20 lg:py-32">
+       <BackgroundBeamsWithCollision className="absolute top-0 left-0 w-full h-full z-0" />
       <div className="relative z-10 text-center my-20">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center mx-auto">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
