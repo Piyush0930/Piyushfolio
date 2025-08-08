abd688f3f88ae06516d429f13821ac9b786b0f63
Piyush Patil <piyushpatil922004@gmail.com>
2025-08-08 06:59:42 +0000

    You are given a task to integrate a React component into your codebase.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,10 @@ import { Education } from "@/components/education";
 import { Contact } from "@/components/contact";
 import { Footer } from "@/components/footer";
 import { navItems, socialLinks } from "@/lib/data.tsx";
-import { BackgroundBeams } from "@/components/ui/background-beams";
 
 export default function Home() {
   return (
     <main className="relative bg-background flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
-      <BackgroundBeams className="absolute top-0 left-0 w-full h-full z-0" />
       <div className="max-w-7xl w-full relative z-10">
         <Header navItems={navItems} />
         <Hero />
